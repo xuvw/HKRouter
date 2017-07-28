64972dafa1de8f5a1d12465036bc837ed7b1903e
ke he <smileshitou@hotmail.com>
2017-07-28 13:24:07 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-HKRouter_Tests/Pods-HKRouter_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HKRouter_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_HKRouter_Tests
+@end
---
+++ Example/Pods/Target Support Files/Pods-HKRouter_Tests/Pods-HKRouter_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_HKRouter_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HKRouter_TestsVersionString[];
+
